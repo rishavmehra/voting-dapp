8428928a0cb16e5da18444dfc115d444c80379c5
Rishav Mehra <rishavmehra61@gmail.com>
2024-12-15 00:49:19 +0530

    new program deploy on devnet

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -2,7 +2,7 @@
 
 use anchor_lang::prelude::*;
 
-declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+declare_id!("oYhe6wjJ8gm2MbfCpfBdwbTvCRwVQ1vmTD9SrJBpoL9");
 
 #[program]
 pub mod votingdapp {
--- anchor/target/types/votingdapp.ts
+++ anchor/target/types/votingdapp.ts
@@ -5,7 +5,7 @@
  * IDL can be found at `target/idl/votingdapp.json`.
  */
 export type Votingdapp = {
-  "address": "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF",
+  "address": "oYhe6wjJ8gm2MbfCpfBdwbTvCRwVQ1vmTD9SrJBpoL9",
   "metadata": {
     "name": "votingdapp",
     "version": "0.1.0",
